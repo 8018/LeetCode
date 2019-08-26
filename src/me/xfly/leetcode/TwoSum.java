ca4e9c32eacdb1e3da975e1d21b3503378e93045
Neo <liuxiangfei29@gmail.com>
2019-08-26 17:04:36 +0800

    添加注释

--- src/me/xfly/leetcode/TwoSum.java
+++ src/me/xfly/leetcode/TwoSum.java
@@ -12,14 +12,14 @@ public class TwoSum {
 	}
 	
 	public static int[] twoSum(int[] nums, int target) {
-
+		//用 HashMap 存储已经遍历数据
 		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 		for (int i = 0; i < nums.length; i++) {
+			//如果当前数据和已经遍历的某个数据和等于 target 返回
 			if (map.containsKey(target - nums[i])) {
-
 				return new int[] { map.get(target - nums[i]), i };
 			}
-
+			//将数据存入 map
 			map.put(nums[i], i);
 		}
 
