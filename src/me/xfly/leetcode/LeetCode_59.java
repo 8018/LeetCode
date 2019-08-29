0d2858de26063b2c4b3c1e355e3b281e56cb2d12
Neo <liuxiangfei29@gmail.com>
2019-08-30 01:18:23 +0800

    填充矩阵

---
+++ src/me/xfly/leetcode/LeetCode_59.java
@@ -0,0 +1,41 @@
+package me.xfly.leetcode;
+
+
+public class LeetCode_59 {
+
+	public static void main(String[] args) {
+		int n = 3;
+		int[][] matrix = generateMatrix(n);
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length; j++) {
+				System.out.print(matrix[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static int[][] generateMatrix(int n) {
+		int l = 0, r = n - 1, t = 0, b = n - 1;
+        int[][] mat = new int[n][n];
+        int num = 1, tar = n * n;
+        while(num <= tar){
+            for(int i = l; i <= r; i++) {
+            	mat[t][i] = num++; // left to right.
+            }
+            t++;
+            for(int i = t; i <= b; i++) {
+            	mat[i][r] = num++; // top to bottom.
+            }
+            r--;
+            for(int i = r; i >= l; i--) {
+            	mat[b][i] = num++; // right to left.
+            }
+            b--;
+            for(int i = b; i >= t; i--) {
+            	mat[i][l] = num++; // bottom to top.
+            }
+            l++;
+        }
+        return mat;
+	}
+}
