b6fec930213a875b0cf3c4058e6822002eb56ae5
neo <liuxiangfei29@gmail.com>
2020-07-16 16:42:26 +0800

    merge

--- src/me/xfly/leetcode/AtomicBooleanTest.java
+++ src/me/xfly/leetcode/AtomicBooleanTest.java
@@ -1,4 +1,41 @@
 package me.xfly.leetcode;
 
-public class AtomicBooleanTest {
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicBooleanTest implements Runnable {
+
+    private volatile static AtomicBoolean flag = new AtomicBoolean(true);
+
+    public static void main(String[] args) {
+
+        AtomicBooleanTest ast = new AtomicBooleanTest();
+        Thread thread1 = new Thread(ast);
+        Thread thread = new Thread(ast);
+        thread1.start();
+        thread.start();
+    }
+    @Override
+    public void run() {
+        System.out.println("thread:"+Thread.currentThread().getName()+";flag:"+flag.get());
+        if (flag.compareAndSet(true,false)){
+            System.out.println(Thread.currentThread().getName()+""+flag.get());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            flag.set(true);
+        }else{
+            System.out.println("重试机制thread:"+Thread.currentThread().getName()+";flag:"+flag.get());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread 推出");
+            //run();
+        }
+
+    }
 }
