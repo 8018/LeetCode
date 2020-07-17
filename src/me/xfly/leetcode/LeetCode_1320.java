165a8bbc7213913bc98bf7b28bc13a9c38fa3230
neo <liuxiangfei29@gmail.com>
2020-07-17 12:57:07 +0800

    手指移动距离 dp

--- src/me/xfly/leetcode/LeetCode_1320.java
+++ src/me/xfly/leetcode/LeetCode_1320.java
@@ -11,45 +11,50 @@ public class LeetCode_1320 {
         System.out.println(minimumDistance(word.toUpperCase()));
     }
 
-    static int distance(char x, char y){
+    static int distance(char x, char y) {
         int m = x - 'A';
         int n = y - 'A';
-        int xdis = abs(m/6-n/6);
-        int ydis = abs((m%6)-(n%6));
+        int xdis = abs(m / 6 - n / 6);
+        int ydis = abs((m % 6) - (n % 6));
         return xdis + ydis;
     }
 
     static int minimumDistance(String word) {
         int len = word.length();
 
-        int[][] d = new int[len][len];      // DP Array
+        int[][] dp = new int[len][len];      // DP Array
         int[][] dist = new int[len][len];   // dist Array，用 tabular 空间换时间避免重复计算，
-        // 但此步意义也不明显，因为重复计算并不频繁，可省略
-        for (int i = 0; i < len; i++)
-            for (int j = i+1; j < len; j++)
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
                 dist[i][j] = distance(word.charAt(i), word.charAt(j));
-
+            }
+        }
+        // 假设现在两个手指分别放在 i 字母处和 j 字母处
+        // 新加入一个字母后 新的手指坐标可能为 (i+1,i) 和 （i+1,j）
+        // (i+1,i) 原来的 j 处的手指移动到 i+1 移动距离为 dist[j][i+1]
+        //（i+1,j） 原来 i 处的手指移动到 i+1 处 移动距离为 dist[i][i+1]
         for (int i = 1; i < len; i++) {
-            Arrays.fill(d[i], Integer.MAX_VALUE);   // 初始化
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
 
-            for (int j = 0; j < i-1; j++) {
-                // 将原先放在第 i-1 个字母上的手指移到 i 上，此时两根手指分别以第 i, j 个字母为终点
-                d[i][j] = Math.min(d[i][j], d[i-1][j] + dist[i-1][i]);
-                // 将原先放在第 j 个字母上的手指移到 i 上，此时两根手指分别以第 i, i-1 个字母为终点
-                d[i][i-1] = Math.min(d[i][i-1], d[i-1][j] + dist[j][i]);
+            for (int j = 0; j < i; j++) {
+                dp[i][j] = Math.min(dp[i][j],dp[i-1][j] + dist[i-1][i]);
+
+                dp[i][i-1] = Math.min(dp[i][i-1],dp[i-1][j]+ dist[j][i]);
             }
 
-            // 如果此前仅使用了一根手指，可以将第二根手指放在第 i 个字母上，不消耗步数；可以将消耗理解为 `d[i-1][i-1] + 0`
-            d[i][i-1] = Math.min(d[i][i-1], d[i-1][i-1]);
-            // 依旧仅使用一根手指
-            d[i][i] = d[i-1][i-1] + dist[i-1][i];
+            // 原来用一根手指，第二根手指放新的字母上
+            dp[i][i-1] = Math.min(dp[i][i-1], dp[i-1][i-1]);
+
+            //依旧用一根手指，不放第二根
+            dp[i][i] = dp[i-1][i-1] + dist[i-1][i];
         }
 
-        int ret = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
         for (int i = 0; i < len-1; i++)
-            ret = Math.min(ret, d[len-1][i]);   // 无需考虑 d[len-1][len-1]，必然不是最优
+            ans = Math.min(ans, dp[len-1][i]);   // 无需考虑 d[len-1][len-1]，必然不是最优
 
-        return ret;
+        return ans;
 
     }
 
